7d0c068840e5d49c05270d79af560df823e1cfc1
rozimuhammad <roziisomiddinov@gmail.com>
2024-02-22 21:50:16 +0500

    for index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const { path, winstonFunction, port } = require("./connectionparams");
 const winston = require("winston");
-const app1 = require("./router");
+const app1 = require("./routers/router");
+const app2 = require("./routers/routerDelete");
 winstonFunction();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(path("auth"), app2);
 app.use(path(), app1);
 
 app.listen(port, () => {
